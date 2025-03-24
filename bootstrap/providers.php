7bb6fe9bcaf2a0a1f6b2d4a22877b1013373ad7b
Leonardo Nunes <leonardo.oliveira@pratadigital.com.br>
2025-03-24 08:40:11 -0300

    Controle de acesso

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-//    App\Providers\CheckExpiredBenefitsProvider::class,
+    App\Providers\CheckExpiredBenefitsProvider::class,
     Jenssegers\Agent\AgentServiceProvider::class
 ];
