cf65e0b688e3dad8993210acdfd081ec28a47f8f
Leonardo Nunes <leonardo.oliveira@pratadigital.com.br>
2025-03-28 23:02:12 -0300

    Ajustes no projeto

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/pusher-listener.js', 'resources/js/push-register.js'],
             refresh: true,
         }),
     ],
